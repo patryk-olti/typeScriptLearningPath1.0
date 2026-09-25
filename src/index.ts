a2dab7b1d9c4bc54517ea4eb41be44359a5e7d92
Patryk Oltuch <95oltuchpatryk@gmail.com>
2026-09-25 14:20:23 +0200

    added userChecker for node.js

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,61 @@
 import { createServer } from "node:http";
+import type { User } from './types/User.js';
+
+function checkUser(value: User | undefined): value is User{
+    if( (typeof(value) !== "object") || (typeof(value) === "undefined") ){ return false; }
+
+    if(!("name" in value)){ return false; }
+
+    if(typeof(value.name) !== "string"){ return false; }
+
+    if(!("age" in value)){ return false; }
+
+    if(typeof(value.age) !== "number"){ return false; }
+
+    return true;
+}
 
 const server = createServer((req, res) => {
     if(req.url === "/"){
-        res.end("Hello from backend!");
-    }else if(req.url === "/users" && req.method === "GET"){
+
         res.writeHead(200);
-        res.end("Users endpoint");
+        res.end("Hello from backend!");
+    
+    }else if(req.url === "/users" && req.method === "GET"){
+
+        res.writeHead(200);
+        res.end("List of users");
+    
+    }else if(req.url === "/users" && req.method === "POST"){
+
+        let body = "";
+
+        req.on("data", (chunk) => {
+            body = body + chunk;
+        })
+
+        req.on("end", () => {
+            const data = JSON.parse(body);
+
+            if(checkUser(data)){
+                res.writeHead(201);
+                res.end(`User created: ${JSON.stringify(data)}`);
+            }else{
+                res.writeHead(400);
+                res.end(`Bad input!`);
+            }
+            
+        })
+
     }else if(req.url === "/about"){
+
+        res.writeHead(200);
         res.end("About endpoint");
+
     }else{
         res.writeHead(404);
         res.end("not found!");
     }
-
 });
 
 
